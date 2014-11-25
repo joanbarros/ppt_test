6ddaa38ac7b87c0c09f7b09b85ca0eac61b8ead9
Joan Barros <barros.joan@gmail.com>
2014-11-25 00:07:37 -0400

    All tests passing.
    The api for the model is a little flawed, I need to revisit that.

--- app/post.rb
+++ app/post.rb
@@ -16,8 +16,43 @@ class Post
   def find_by_id id
     @data.each do |item|
       if item["id"].to_i == id
-        item
+        return item
       end
     end
+  end
+
+  def find_by_author author
+    posts = []
+    @data.each do |item|
+      if item["author"] == author
+        posts.push item
+      end
+    end
+    posts
+  end
+  
+  def find_by_tag tag
+    posts = []
+    @data.each do |item|
+      if item["tags"].tr(' ', '').split(',').include? tag
+        posts.push item
+      end
+    end
+    return posts
+  end
+
+  def get_tags
+    tags = []
+    @data.each do |item|
+      item["tags"].tr(" ", "").split(',').each do |tag|
+        tags.push tag unless tags.include? tag
+      end
+    end
+    return tags
+  end
+  
+  def description_preview post
+    #post = self.find_by_id id
+    post["description"].split(' ')[0,10].join(' ')
   end
 end
